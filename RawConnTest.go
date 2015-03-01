347f852d5ec1cd396c2f3b7d796bebd75f943cbc
omegablitz <aashishwelling@gmail.com>
2015-03-01 05:04:58 +0000

    Another mistake fixed

--- RawConnTest.go
+++ RawConnTest.go
@@ -50,7 +50,7 @@ func (c *UDP) read(size int) ([]byte, error) {
 func (c *UDP) write(x []byte) error {
 	UDPHeader := []byte{
 		c.src >> 8, c.src << 8 >> 8, // Source port in byte slice
-		c.dest >> 8, c.src << 8 >> 8, // Dest port in byte slice
+		c.dest >> 8, c.dest << 8 >> 8, // Dest port in byte slice
 		(8 + len(x)) >> 8, (8 + len(x)) << 8 >> 8, // Length in bytes of UDP header + data
 		0, 0, // Checksum
 	}
