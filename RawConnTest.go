1844884eaec4a02460d6b1c8f2521eaaae4b8a2f
Harshal Sheth <hsheth2@gmail.com>
2015-03-08 13:20:58 -0400

    Put back the loop

--- RawConnTest.go
+++ RawConnTest.go
@@ -30,7 +30,7 @@ func main() {
 
     manager, _ := NewUDP_Manager("127.0.0.1")
     c := []*UDP{}
-    for i := 0; i < 1; i++ {
+    for i := 0; i < 20; i++ {
         udp, _ := manager.NewUDP(((uint16)(20000 + i)), ((uint16)(20000 + i)))
         c = append(c, udp)
         udp.write([]byte{((byte)(100 + i))})
