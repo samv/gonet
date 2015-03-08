e2a2f7b7de86bf462ffbf7848ce80d57aa68cde8
Harshal Sheth <hsheth2@gmail.com>
2015-03-07 19:20:44 -0500

    Formatted the code

--- RawConnTest.go
+++ RawConnTest.go
@@ -1,135 +1,135 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/net/ipv4"
-	"net"
+    "fmt"
+    "golang.org/x/net/ipv4"
+    "net"
 )
 
 func main() {
-	manager, _ := NewUDP_Manager("127.0.0.1")
-	c := []*UDP{}
-	for i := 0; i < 20; i++ {
-		udp, _ := manager.NewUDP(((uint16)(20000 + i)), ((uint16)(20000 + i)))
-		c = append(c, udp)
-		udp.write([]byte{((byte)(100 + i))})
-		a, _ := udp.read(1)
-		fmt.Println(a)
-	}
-	for _, element := range c {
-		element.close()
-	}
+    manager, _ := NewUDP_Manager("127.0.0.1")
+    c := []*UDP{}
+    for i := 0; i < 20; i++ {
+        udp, _ := manager.NewUDP(((uint16)(20000 + i)), ((uint16)(20000 + i)))
+        c = append(c, udp)
+        udp.write([]byte{((byte)(100 + i))})
+        a, _ := udp.read(1)
+        fmt.Println(a)
+    }
+    for _, element := range c {
+        element.close()
+    }
 }
 
 type UDP_manager struct {
     ipAddress string
-	pl   net.PacketConn
-	open bool
-	conn *ipv4.RawConn
-	buff map[uint16](chan byte)
+    pl        net.PacketConn
+    open      bool
+    conn      *ipv4.RawConn
+    buff      map[uint16](chan byte)
 }
 
 type UDP struct {
     manager   *UDP_manager
-	conn      *ipv4.RawConn
-	bytes     chan byte
-	src, dest uint16
+    conn      *ipv4.RawConn
+    bytes     chan byte
+    src, dest uint16
 }
 
 func NewUDP_Manager(ip string) (*UDP_manager, error) {
-	p, err := net.ListenPacket("ip4:17", ip)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
+    p, err := net.ListenPacket("ip4:17", ip)
+    if err != nil {
+        fmt.Println(err)
+        return nil, err
+    }
 
-	r, err := ipv4.NewRawConn(p)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
+    r, err := ipv4.NewRawConn(p)
+    if err != nil {
+        fmt.Println(err)
+        return nil, err
+    }
 
-	x := &UDP_manager{open: true, conn: r, pl: p, buff: make(map[uint16](chan byte)), ipAddress: ip}
+    x := &UDP_manager{open: true, conn: r, pl: p, buff: make(map[uint16](chan byte)), ipAddress: ip}
 
-	go x.readAll()
+    go x.readAll()
 
-	return x, nil
+    return x, nil
 }
 
 func (x *UDP_manager) readAll() {
-	b := make([]byte, 1024)
+    b := make([]byte, 1024)
 
-	for {
-		_, payload, _, err := x.conn.ReadFrom(b)
-		if err != nil {
-			continue
-		}
+    for {
+        _, payload, _, err := x.conn.ReadFrom(b)
+        if err != nil {
+            continue
+        }
 
-		dest := (((uint16)(payload[2])) << 8) + ((uint16)(payload[3]))
-		//		fmt.Println(dest)
-		//		fmt.Println(payload)
-		//
-		//		fmt.Println(x.buff)
-		c, ok := x.buff[dest]
-		//fmt.Println(ok)
-		payload = payload[8:]
-		if ok {
-			go func() {
-				for _, elem := range payload {
-					//fmt.Println("Writing")
-					c <- elem
-				}
-			}()
-		}
-	}
+        dest := (((uint16)(payload[2])) << 8) + ((uint16)(payload[3]))
+        //		fmt.Println(dest)
+        //		fmt.Println(payload)
+        //
+        //		fmt.Println(x.buff)
+        c, ok := x.buff[dest]
+        //fmt.Println(ok)
+        payload = payload[8:]
+        if ok {
+            go func() {
+                for _, elem := range payload {
+                    //fmt.Println("Writing")
+                    c <- elem
+                }
+            }()
+        }
+    }
 }
 
 func (x *UDP_manager) NewUDP(src, dest uint16) (*UDP, error) {
-	x.buff[src] = make(chan byte, 1024)
-	return &UDP{src: src, dest: dest, conn: x.conn, bytes: x.buff[src], manager: x}, nil
+    x.buff[src] = make(chan byte, 1024)
+    return &UDP{src: src, dest: dest, conn: x.conn, bytes: x.buff[src], manager: x}, nil
 }
 
 func (c *UDP) read(size int) ([]byte, error) {
-	data := make([]byte, size)
-	for i := 0; i < size; i++ {
-		//fmt.Println("test")
-		data[i] = <-c.bytes
-		//fmt.Println(data[i])
-	}
-	return data, nil
+    data := make([]byte, size)
+    for i := 0; i < size; i++ {
+        //fmt.Println("test")
+        data[i] = <-c.bytes
+        //fmt.Println(data[i])
+    }
+    return data, nil
 }
 func (c *UDP) write(x []byte) error {
-	UDPHeader := []byte{
-		(byte)(c.src >> 8), (byte)(c.src), // Source port in byte slice
-		(byte)(c.dest >> 8), (byte)(c.dest), // Dest port in byte slice
-		(byte)((8 + len(x)) >> 8), (byte)(8 + len(x)), // Length in bytes of UDP header + data
-		0, 0, // Checksum
-	}
+    UDPHeader := []byte{
+        (byte)(c.src >> 8), (byte)(c.src), // Source port in byte slice
+        (byte)(c.dest >> 8), (byte)(c.dest), // Dest port in byte slice
+        (byte)((8 + len(x)) >> 8), (byte)(8 + len(x)), // Length in bytes of UDP header + data
+        0, 0, // Checksum
+    }
 
-	x = append(UDPHeader, x...)
+    x = append(UDPHeader, x...)
 
-	h := &ipv4.Header{
-		Version:  ipv4.Version,      // protocol version
-		Len:      20,                // header length
-		TOS:      0,                 // type-of-service (0 is everything normal)
-		TotalLen: len(x) + 20,       // packet total length (octets)
-		ID:       0,                 // identification
-		Flags:    ipv4.DontFragment, // flags
-		FragOff:  0,                 // fragment offset
-		TTL:      8,                 // time-to-live (maximum lifespan in seconds)
-		Protocol: 17,                // next protocol (17 is UDP)
-		Checksum: 0,                 // checksum (apparently autocomputed)
-		//Src:    net.IPv4(127, 0, 0, 1), // source address, apparently done automatically
-		Dst: net.ParseIP(c.manager.ipAddress), // destination address
-		//Options                         // options, extension headers
-	}
-	err := c.conn.WriteTo(h, x, nil)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	return nil
+    h := &ipv4.Header{
+        Version:  ipv4.Version, // protocol version
+        Len:      20, // header length
+        TOS:      0, // type-of-service (0 is everything normal)
+        TotalLen: len(x) + 20, // packet total length (octets)
+        ID:       0, // identification
+        Flags:    ipv4.DontFragment, // flags
+        FragOff:  0, // fragment offset
+        TTL:      8, // time-to-live (maximum lifespan in seconds)
+        Protocol: 17, // next protocol (17 is UDP)
+        Checksum: 0, // checksum (apparently autocomputed)
+        //Src:    net.IPv4(127, 0, 0, 1), // source address, apparently done automatically
+        Dst: net.ParseIP(c.manager.ipAddress), // destination address
+        //Options                         // options, extension headers
+    }
+    err := c.conn.WriteTo(h, x, nil)
+    if err != nil {
+        fmt.Println(err)
+        return err
+    }
+    return nil
 }
 func (c *UDP) close() error {
-	return c.conn.Close()
+    return c.conn.Close()
 }
--- ipv4.go
+++ ipv4.go
@@ -1,14 +1,17 @@
 package main
-import "net"
+
+import (
+    "net"
+)
 
 type IP_Conn struct {
-    pc *net.PacketConn
-    version uint8
-    dst, src string
+    pc        *net.PacketConn
+    version   uint8
+    dst, src  string
     headerLen uint16
     //len uint16
     //id uint16
-    ttl uint8
+    ttl      uint8
     protocol uint8
     //checksum int
 }
@@ -16,34 +19,34 @@ type IP_Conn struct {
 func NewIP_Conn(dst string) (*IP_Conn, error) {
     pc, _ := net.ListenPacket("ip4", dst)
     return &IP_Conn{
-        pc: &pc,
-        version: 4,
+        pc:        &pc,
+        version:   4,
         headerLen: 20,
-        dst:     dst,
-        src:     "127.0.0.1",
-        ttl:     8,
-        protocol: 17,
+        dst:       dst,
+        src:       "127.0.0.1",
+        ttl:       8,
+        protocol:  17,
     }, nil
 }
 
 func calcChecksum(head []byte, excludeChecksum bool) uint16 {
-    totlSum := uint64(0)
+    totalSum := uint64(0)
     for ind, elem := range head {
         if (ind == 10 || ind == 11) && excludeChecksum { // Ignore the checksum in some situations
             continue
         }
 
-        if ind % 2 == 0 {
-            totlSum += (uint64)(uint16(elem) << 8)
+        if ind%2 == 0 {
+            totalSum += (uint64)(uint16(elem) << 8)
         } else {
-            totlSum += (uint64)(uint16(elem))
+            totalSum += (uint64)(uint16(elem))
         }
     }
 
-    for prefix := (totlSum >> 16); prefix != 0; {
-        totlSum = uint64(uint16(totlSum)) + prefix
+    for prefix := (totalSum >> 16); prefix != 0; {
+        totalSum = uint64(uint16(totalSum)) + prefix
     }
-    carried := uint16(totlSum)
+    carried := uint16(totalSum)
 
     return ^carried
 }
@@ -61,9 +64,9 @@ func (ipc *IP_Conn) WriteTo(p []byte) {
     packet[3] = (byte)(totalLen)
     packet[4] = 0 // Identification (for now)
     packet[5] = 0
-    packet[6] = byte(1 << 6) // Flags: Don't fragment
-    packet[7] = 0 // Fragment Offset
-    packet[8] = (byte)(ipc.ttl) // Time to Live
+    packet[6] = byte(1 << 6)         // Flags: Don't fragment
+    packet[7] = 0                    // Fragment Offset
+    packet[8] = (byte)(ipc.ttl)      // Time to Live
     packet[9] = (byte)(ipc.protocol) // Protocol
 
     // Src and Dst IPs
@@ -98,18 +101,18 @@ func (ipc *IP_Conn) Close() error {
 }
 
 /* h := &ipv4.Header{
-		Version:  ipv4.Version,      // protocol version
-		Len:      20,                // header length
-		TOS:      0,                 // type-of-service (0 is everything normal)
-		TotalLen: len(x) + 20,       // packet total length (octets)
-		ID:       0,                 // identification
-		Flags:    ipv4.DontFragment, // flags
-		FragOff:  0,                 // fragment offset
-		TTL:      8,                 // time-to-live (maximum lifespan in seconds)
-		Protocol: 17,                // next protocol (17 is UDP)
-		Checksum: 0,                 // checksum (apparently autocomputed)
-		//Src:    net.IPv4(127, 0, 0, 1), // source address, apparently done automatically
-		Dst: net.ParseIP(c.manager.ipAddress), // destination address
-		//Options                         // options, extension headers
-	}
-	*/
+	Version:  ipv4.Version,      // protocol version
+	Len:      20,                // header length
+	TOS:      0,                 // type-of-service (0 is everything normal)
+	TotalLen: len(x) + 20,       // packet total length (octets)
+	ID:       0,                 // identification
+	Flags:    ipv4.DontFragment, // flags
+	FragOff:  0,                 // fragment offset
+	TTL:      8,                 // time-to-live (maximum lifespan in seconds)
+	Protocol: 17,                // next protocol (17 is UDP)
+	Checksum: 0,                 // checksum (apparently autocomputed)
+	//Src:    net.IPv4(127, 0, 0, 1), // source address, apparently done automatically
+	Dst: net.ParseIP(c.manager.ipAddress), // destination address
+	//Options                         // options, extension headers
+}
+*/
