442b6095d40315167f752301726e17317f061598
Harshal Sheth <hsheth2@gmail.com>
2015-03-15 14:51:47 -0400

    automatic commit

--- udpWrite.py
+++ udpWrite.py
@@ -1,17 +1,17 @@
 import socket
 import sys
 
-UDP_IP = "127.0.0.1"
+DST_IP = "127.0.0.1"
 if len(sys.argv) <= 1:
     UDP_PORT = 20000
 else:
     UDP_PORT = int(sys.argv[1])
 MESSAGE = "Hello, World!"
 
-print "UDP target IP:", UDP_IP
+print "UDP target IP:", DST_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE, (DST_IP, UDP_PORT))
