f17b9ceb4234f06fd23f60b6d4bd66c68c12c099
Aashish <aashishwelling@gmail.com>
2015-03-23 23:14:21 -0400

    Checksum is the problem... tons of random debug messages, sorry.

--- udp_reader.go
+++ udp_reader.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "errors"
+	"errors"
+	"fmt"
 )
 
 const MAX_UDP_PACKET_LEN = 65507
 
 type UDP_Read_Manager struct {
-	reader    *IP_Reader
-	buff      map[uint16](map[string](chan []byte))
+	reader *IP_Reader
+	buff   map[uint16](map[string](chan []byte))
 }
 
 type UDP_Reader struct {
-	manager *UDP_Read_Manager
-	bytes   <-chan []byte
-	port    uint16 // ports
-    ipAddress string
+	manager   *UDP_Read_Manager
+	bytes     <-chan []byte
+	port      uint16 // ports
+	ipAddress string
 }
 
 func NewUDP_Read_Manager() (*UDP_Read_Manager, error) {
@@ -31,8 +31,8 @@ func NewUDP_Read_Manager() (*UDP_Read_Manager, error) {
 	}
 
 	x := &UDP_Read_Manager{
-		reader:    ipr,
-		buff:      make(map[uint16](map[string](chan []byte))),
+		reader: ipr,
+		buff:   make(map[uint16](map[string](chan []byte))),
 	}
 
 	go x.readAll()
@@ -50,21 +50,21 @@ func (x *UDP_Read_Manager) readAll() {
 		//fmt.Println("UDP header and payload: ", payload)
 
 		dst := (((uint16)(payload[2])) * 256) + ((uint16)(payload[3]))
-        //fmt.Println(dst)
+		//fmt.Println(dst)
 
-        payload = payload[8:]
+		payload = payload[8:]
 		//fmt.Println(payload)
 
 		portBuf, ok := x.buff[dst]
 		//fmt.Println(ok)
 		if ok {
-            if c, ok := portBuf[ip]; ok {
-                fmt.Println("Found exact IP match for port", dst)
-                go func() { c <- payload }()
-            } else if c, ok := portBuf["*"]; ok {
-                //fmt.Println("Found default IP match for port", dst)
-                go func() { c <- payload }()
-            }
+			if c, ok := portBuf[ip]; ok {
+				fmt.Println("Found exact IP match for port", dst)
+				go func() { c <- payload }()
+			} else if c, ok := portBuf["*"]; ok {
+				//fmt.Println("Found default IP match for port", dst)
+				go func() { c <- payload }()
+			}
 		} else {
 			// drop packet
 		}
@@ -72,18 +72,18 @@ func (x *UDP_Read_Manager) readAll() {
 }
 
 func (x *UDP_Read_Manager) NewUDP(port uint16, ip string) (*UDP_Reader, error) {
-    // add the port if not already there
+	// add the port if not already there
 	if _, found := x.buff[port]; !found {
-        x.buff[port] = make(map[string](chan []byte))
-    }
+		x.buff[port] = make(map[string](chan []byte))
+	}
 
-    // add the ip to the port's list
-    if _, found := x.buff[port][ip]; !found {
-        x.buff[port][ip] = make(chan []byte)
-        return &UDP_Reader{port: port, bytes: x.buff[port][ip], manager: x, ipAddress: ip}, nil
-    } else {
-        return nil, errors.New("Another application is already listening to that port.")
-    }
+	// add the ip to the port's list
+	if _, found := x.buff[port][ip]; !found {
+		x.buff[port][ip] = make(chan []byte)
+		return &UDP_Reader{port: port, bytes: x.buff[port][ip], manager: x, ipAddress: ip}, nil
+	} else {
+		return nil, errors.New("Another application is already listening to that port.")
+	}
 }
 
 func (c *UDP_Reader) read(size int) ([]byte, error) {
