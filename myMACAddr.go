91ad243b9826f02d363ccee1a2768acba07e89a0
Harshal Sheth <hsheth2@gmail.com>
2015-03-25 21:33:28 +0000

    myMACAddr.go deleted online with Bitbucket

--- myMACAddr.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// this is used as both source and destination MAC
-var myMACAddr = [8]byte{ 0xb4, 0x75, 0x0e, 0x23, 0x5c, 0x87, // actual MAC address
-    0x00, 0x00, // not used
-}
-
-var MyIfIndex = int(4)
