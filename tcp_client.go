b440d05943998838c70de681a2aa23fc2220ed9b
Aashish <aashishwelling@gmail.com>
2015-04-05 22:09:46 -0400

    Pretty sure I missed a ton

--- tcp_client.go
+++ tcp_client.go
@@ -1,1 +1,28 @@
 package main
+
+type TCP_Client struct {
+	ipAddress string // destination ip address
+	writer    *IP_Writer
+	src, dst  uint16 // ports
+}
+
+func New_TCP_Client(src, dest uint16, dstIP string) (*TCP_Client, error) {
+	write, err := NewIP_Writer(dstIP, 6)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TCP_Client{src: src, dst: dest, ipAddress: dstIP, writer: write}, nil
+}
+
+func (c *TCP_Client) connect() error {
+
+}
+
+func (c *TCP_Client) write() error {
+
+}
+
+func (c *TCP_Client) close() error {
+	return nil
+}
--- tcp_server.go
+++ tcp_server.go
@@ -1,1 +1,34 @@
 package main
+
+type TCP_Server struct {
+}
+
+type TCP_Server_Manager struct {
+	reader  *IP_Reader
+	servers map[uint16](map[string](chan []byte))
+}
+
+func New_TCP_Server_Manager() (*TCP_Server_Manager, error) {
+	nr, err := NewNetwork_Reader()
+	if err != nil {
+		return nil, err
+	}
+
+	ipr, err := nr.NewIP_Reader("*", 6)
+	if err != nil {
+		return nil, err
+	}
+
+	x := &TCP_Server_Manager{
+		reader:  ipr,
+		servers: make(map[uint16](map[string](chan []byte))),
+	}
+
+	return x, nil
+}
+
+func (*TCP_Server_Manager) Listen(port uint16, ip string) (*TCP_Server, error) {
+
+}
+
+func (*TCP_Server) Close() {}
