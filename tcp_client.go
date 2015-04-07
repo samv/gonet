eaf1e33ce6b5f3a870a1c875340d9e062dc9808e
Aashish <aashishwelling@gmail.com>
2015-04-07 18:26:15 -0400

    Finished most of init SYN Added some TODOs

--- tcp_client.go
+++ tcp_client.go
@@ -16,8 +16,37 @@ func New_TCP_Client(src, dest uint16, dstIP string) (*TCP_Client, error) {
 }
 
 func (c *TCP_Client) Connect() error {
-	// TODO Send SYN
+	// SYN
+	initSeqNumber := uint32(0) // Can be any number between 0 and 2^32 inclusive, TODO this needs to be changed later, it can't be predictable.
+	initAckNumber := uint32(0) // Always 0 at start
+	window := uint16(2 ^ 10)   // TODO calc using http://ithitman.blogspot.com/2013/02/understanding-tcp-window-window-scaling.html
+	SYN := []byte{
+		(byte)(c.src >> 8), (byte)(c.src), // Source port in byte
+		(byte)(c.dst >> 8), (byte)(c.dst), // Destination port in byte slice
+		(byte)(initSeqNumber >> 24), (byte)(initSeqNumber >> 16), (byte)(initSeqNumber >> 8), (byte)(initSeqNumber),
+		(byte)(initAckNumber >> 24), (byte)(initAckNumber >> 16), (byte)(initAckNumber >> 8), (byte)(initAckNumber),
+		(byte)(
+			5 << 4, // Size of header in 32 bit chunks. It is always 5 unless options are used. This is also the data offset.
+			// bits 5-7 inclusive are reserved, always 0
+			// bit 8 is flag 0(NS flag), set to 0 here because only SYN
+		),
+		(byte)( // Flags 1-8 inclusive
+			// CWR
+			// ECE
+			// URG
+			// ACK
+			// PSH
+			// RST
+			1 << 1, //SYN
+			// FIN
+		),
+		(byte)(window >> 8), (byte)(window),
+		0, 0, // TODO calc checksum, right now just set to 0
+		0, 0, // URG pointer, only matters where URG flag is set.
+	}
+
 	// TODO Wait for SYN + ACK, send back ACK
+
 	return nil
 }
 
