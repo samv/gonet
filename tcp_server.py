3cc6450020f74e51824c517c8208b58c2fe97b80
Harshal Sheth <hsheth2@gmail.com>
2015-04-08 18:18:23 -0400

    Edits to the python scripts

--- tcp_server.py
+++ tcp_server.py
@@ -8,14 +8,23 @@ TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Created socket"
+
 s.bind((TCP_IP, TCP_PORT))
+print "Binded"
+
 s.listen(1)
+print "Listened"
 
 conn, addr = s.accept()
+print "Accepted"
+
 print 'Connection address:', addr
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
     print "received data:", data
     conn.send(data)  # echo
+
+print "Closing"
 conn.close()
