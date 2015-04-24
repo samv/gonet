070124e1bf37b7e257c834c39d959343037b51f0
Harshal Sheth <hsheth2@gmail.com>
2015-04-24 13:21:24 -0400

    Organization. I am working on the use of sync.Cond in the connect function of the tcp_client.go

--- tcp_client.go
+++ tcp_client.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"golang.org/x/net/ipv4"
 	"net"
+	"errors"
+	"time"
 )
 
 func New_TCB_From_Client(local, remote uint16, dstIP string) (*TCB, error) {
@@ -32,4 +34,48 @@ func New_TCB_From_Client(local, remote uint16, dstIP string) (*TCB, error) {
 
 	fmt.Println("Finished New TCB from Client")
 	return New_TCB(local, remote, dstIP, read, r, TCP_CLIENT)
+}
+
+func (c *TCB) Connect() error {
+	if c.kind != TCP_CLIENT || c.state != CLOSED {
+		return errors.New("TCB is not a closed client")
+	}
+	// Send the SYN packet
+	SYN, err := (&TCP_Header{
+		srcport: c.lport,
+		dstport: c.rport,
+		seq:     c.seqNum,
+		ack:     c.ackNum,
+		flags:   TCP_SYN,
+		window:  c.curWindow, // TODO improve the window size calculation
+		urg:     0,
+		options: []byte{0x02, 0x04, 0xff, 0xd7, 0x04, 0x02, 0x08, 0x0a, 0x02, 0x64, 0x80, 0x8b, 0x0, 0x0, 0x0, 0x0, 0x01, 0x03, 0x03, 0x07}, // TODO compute the options of SYN instead of hardcoding them
+	}).Marshal_TCP_Header(c.ipAddress, c.srcIP)
+	if err != nil {
+		return err
+	}
+
+	//c.writer.WriteTo(SYN)
+	err = MyRawConnTCPWrite(c.writer, SYN, c.ipAddress)
+	fmt.Println("Sent SYN")
+	if err != nil {
+		return err
+	}
+	c.UpdateState(SYN_SENT)
+
+	// TODO set up resend SYN timers
+
+	// wait for the connection state to be ready
+	// TODO use sync.Cond broadcast to avoid the infinite for loop
+	for {
+		st := c.state
+		if st == CLOSED {
+			return errors.New("Connection timed out and closed, or reset.")
+		} else if st == ESTABLISHED {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return nil
 }
