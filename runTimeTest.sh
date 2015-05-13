111c79ce508804e9e77b0812e7aa3d24d1799f02
Harshal Sheth <hsheth2@gmail.com>
2015-05-13 19:05:25 -0400

    Updated script

--- runTimeTest.sh
+++ runTimeTest.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
+
+./udp_read_tester >> outputOurs.txt & : ;
+
 for i in `seq 1 10`; do
-	./udp_read_tester &
-	python udpWrite.py 2010 $i
-	echo $i
-done
-python timeTest.py Ours
+	python udpWrite.py 2010 $i >> inputOurs.txt;
+	echo $i;
+done;
+
+python timeTest.py Ours;
+
+kill %%; # kill the udp_read_tester
