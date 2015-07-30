5b51cb6a25a90bdd5817811b0bbbc63317efed93
Harshal Sheth <hsheth2@gmail.com>
2015-07-30 17:45:40 -0400

    Removed all the old test files and update the .gitignore

--- runGoRoutineTests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for c in `seq 1 200`; do
-	echo "New Number of Connections" $c;
-	for t in `seq 1 5`; do
-		echo "Trial" $t;
-		sudo ./udp_read_tester $c >> "TestResults.txt"
-	done
-done
--- u.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm outputOurs.txt
-./udp_read_tester >> outputOurs.txt
