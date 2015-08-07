5c681f74a5afe157216a2dedd4ecf7b47d507c77
Aashish <aashishwelling@gmail.com>
2015-08-07 18:26:10 -0400

    Beginning of timeout swag

--- ping/ping.go
+++ ping/ping.go
@@ -1,17 +1,18 @@
 package ping
 
 import (
+	"bytes"
+	"github.com/hsheth2/logs"
 	"network/icmpp"
 	"network/ipv4p"
 	"time"
-
-	"github.com/hsheth2/logs"
 )
 
 const (
 	PING_ECHO_REQUEST_TYPE = 8
 	PING_ECHO_REPLY_TYPE   = 0
 	PING_ICMP_CODE         = 0
+	PING_START_ID          = 8000
 )
 
 type Ping_Manager struct {
@@ -20,7 +21,9 @@ type Ping_Manager struct {
 	output map[string](*ipv4p.IP_Writer)
 
 	// Sending pings
-	reply chan *icmpp.ICMP_In
+	reply             chan *icmpp.ICMP_In
+	currentIdentifier uint16
+	identifiers       map[uint16](chan *icmpp.ICMP_In)
 }
 
 func NewPing_Manager(icmprm *icmpp.ICMP_Read_Manager) (*Ping_Manager, error) {
@@ -35,9 +38,11 @@ func NewPing_Manager(icmprm *icmpp.ICMP_Read_Manager) (*Ping_Manager, error) {
 	}
 
 	pm := &Ping_Manager{
-		input: input,
-		output: make(map[string](*ipv4p.IP_Writer)),
-		reply: reply,
+		input:             input,
+		output:            make(map[string](*ipv4p.IP_Writer)),
+		reply:             reply,
+		currentIdentifier: PING_START_ID,
+		identifiers:       make(map[uint16](chan *icmpp.ICMP_In)),
 	}
 
 	go pm.ping_replier()
@@ -64,6 +69,19 @@ func (pm *Ping_Manager) getIP_Writer(ip string) (*ipv4p.IP_Writer, error) {
 	return pm.output[ip], nil
 }
 
+func (pm *Ping_Manager) ping_response_dealer() {
+	for {
+		ping := <-pm.reply
+		// TODO verify checksum as well
+		identNum := uint16(ping.Header.Opt >> 16)
+		if _, ok := pm.identifiers[identNum]; !ok {
+			logs.Info.Println("Dropped something from response dealer")
+			continue
+		}
+		pm.identifiers[identNum] <- ping
+	}
+}
+
 func (pm *Ping_Manager) ping_replier() {
 	for {
 		ping := <-pm.input
@@ -72,6 +90,7 @@ func (pm *Ping_Manager) ping_replier() {
 			logs.Error.Println(err)
 			continue
 		}
+		logs.Info.Println("replying:", ping)
 		go pm.respondTo(wr, ping)
 	}
 }
@@ -95,34 +114,82 @@ func (pm *Ping_Manager) respondTo(writer *ipv4p.IP_Writer, ping *icmpp.ICMP_In) 
 	return nil
 }
 
-func (pm *Ping_Manager) SendPing(ip string, interval time.Duration) error {
+func (pm *Ping_Manager) SendPing(ip string, interval, timeout time.Duration, numPings uint16) error {
+	pm.currentIdentifier++
 	ipWriter, err := pm.getIP_Writer(ip)
 	if err != nil {
 		logs.Error.Println(err)
 		return err
 	}
 
-	// prepare packet
-	packet := &icmpp.ICMP_Header{
-		TypeF: PING_ECHO_REQUEST_TYPE,
-		Code:  PING_ICMP_CODE,
-		Opt:   45<<16 | 1,
-		Data:  []byte("abcdefg"),
-	}
+	channel := make(chan *icmpp.ICMP_In)
+	pm.identifiers[pm.currentIdentifier] = channel
 
-	// make data
-	data, err := packet.MarshalICMPHeader()
-	if err != nil {
-		logs.Error.Println(err)
-		return err
-	}
+	seqChannel := make(map[uint16](chan *icmpp.ICMP_In))
 
-	// send
-	err = ipWriter.WriteTo(data)
-	if err != nil {
-		logs.Error.Println(err)
-		return err
-	}
+	go func() {
+		for i := uint16(1); i <= numPings; i++ {
+			// prepare packet
+			packet := &icmpp.ICMP_Header{
+				TypeF: PING_ECHO_REQUEST_TYPE,
+				Code:  PING_ICMP_CODE,
+				Opt:   uint32(pm.currentIdentifier<<16 | i),
+				Data:  []byte("abcdefg"), // TODO legitify
+			}
 
+			// make data
+			data, err := packet.MarshalICMPHeader()
+			if err != nil {
+				logs.Error.Println(err)
+				return
+			}
+
+			// send
+			err = ipWriter.WriteTo(data)
+			if err != nil {
+				logs.Error.Println(err)
+				return
+			}
+			go func(seqChan chan *icmpp.ICMP_In, timeout time.Duration, header *icmpp.ICMP_Header) {
+				time1 := time.Now()
+				timeoutTimer := time.NewTimer(timeout)
+				for {
+					select {
+					case pingResonse := <-seqChan:
+						if !bytes.Equal(pingResonse.Header.Data, header.Data) {
+							logs.Info.Println("Dropped packet cuz data !=")
+							continue
+						}
+						time2 := time.Now()
+						logs.Info.Printf("%d bytes from %s: icmp_seq=%d time=%f ms", len(header.Data)+icmpp.ICMP_Header_MinSize,
+							pingResonse.RIP,
+							uint16(header.Opt),
+							time2.Sub(time1).Seconds()/1000) // put ttl
+					case <-timeoutTimer.C:
+						logs.Info.Println("Seq num of", uint16(header.Opt), "timed out")
+						return
+					}
+				}
+			}(seqChannel[i], timeout, packet)
+
+			// not last
+			if i != numPings {
+				time.Sleep(interval)
+			}
+		}
+	}()
+
+	go func(inChan chan *icmpp.ICMP_In, seqChan map[uint16](chan *icmpp.ICMP_In)) {
+		// TODO verify IPs
+		for {
+			icmp_in := <-inChan
+			if _, ok := seqChan[uint16(icmp_in.Header.Opt)]; !ok {
+				seqChan[uint16(icmp_in.Header.Opt)] = make(chan *icmpp.ICMP_In)
+			}
+			seqChan[uint16(icmp_in.Header.Opt)] <- icmp_in
+		}
+	}(channel, seqChannel)
+
+	time.Sleep(time.Duration(numPings) * timeout)
 	return nil
 }
--- ping/ping_test.go
+++ ping/ping_test.go
@@ -8,11 +8,12 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	err := GlobalPingManager.SendPing("127.0.0.1", time.Second)
+	err := GlobalPingManager.SendPing("127.0.0.1", time.Second, time.Second, 5)
 	if err != nil {
 		logs.Error.Println(err)
 		t.Error(err)
 	} else {
 		t.Log("Success")
 	}
+	time.Sleep(500 * time.Millisecond)
 }
