b7f3201989f4c31f49ff5d7558ebb3eca95a8039
Harshal Sheth <hsheth2@gmail.com>
2015-08-24 19:09:29 -0400

    Made ping replies use the original packet

--- icmp/icmp_utils.go
+++ icmp/icmp_utils.go
@@ -16,20 +16,31 @@ type ICMP_Header struct {
 }
 
 type ICMP_In struct {
-	Header   *ICMP_Header
-	LIP, RIP *ipv4tps.IPaddress
+	Header         *ICMP_Header
+	OriginalPacket []byte
+	LIP, RIP       ipv4tps.IPaddress
 }
 
 func (h *ICMP_Header) MarshalICMPHeader() ([]byte, error) {
-	h.cksum = 0
-	marshaledHeader := append([]byte{
-		(byte)(h.TypeF), (byte)(h.Code), (byte)(h.cksum >> 8), (byte)(h.cksum),
-		(byte)(h.Opt >> 24), (byte)(h.Opt >> 16), (byte)(h.Opt >> 8), (byte)(h.Opt),
-	}, h.Data...)
-	h.cksum = ipv4.Checksum(marshaledHeader)
-	marshaledHeader[2] = byte(h.cksum >> 8)
-	marshaledHeader[3] = byte(h.cksum)
-	return marshaledHeader, nil
+	base := make([]byte, ICMP_Header_MinSize+len(h.Data))
+	copy(base[ICMP_Header_MinSize:], h.Data)
+	err := h.MarshalICMPHeaderGivenSlice(base)
+	return base, err
+}
+
+func (h *ICMP_Header) MarshalICMPHeaderGivenSlice(base []byte) error {
+	base[0] = (byte)(h.TypeF)
+	base[1] = (byte)(h.Code)
+	base[2] = 0 // checksum; set later
+	base[3] = 0
+	base[4] = (byte)(h.Opt >> 24)
+	base[5] = (byte)(h.Opt >> 16)
+	base[6] = (byte)(h.Opt >> 8)
+	base[7] = (byte)(h.Opt)
+	h.cksum = ipv4.Checksum(base)
+	base[2] = byte(h.cksum >> 8)
+	base[3] = byte(h.cksum)
+	return nil
 }
 
 func ExtractICMPHeader(dat []byte, lip, rip *ipv4tps.IPaddress) (*ICMP_In, error) {
@@ -42,7 +53,8 @@ func ExtractICMPHeader(dat []byte, lip, rip *ipv4tps.IPaddress) (*ICMP_In, error
 			Opt:   uint32(dat[4])<<24 | uint32(dat[5])<<16 | uint32(dat[6])<<8 | uint32(dat[7]),
 			Data:  dat[ICMP_Header_MinSize:],
 		},
-		LIP: lip,
-		RIP: rip,
+		LIP:            lip,
+		RIP:            rip,
+		OriginalPacket: dat,
 	}, nil
 }
