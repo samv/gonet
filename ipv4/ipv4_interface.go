7a319ecc677783022145c87bfbcbe4b7ed80c7e2
Harshal Sheth <hsheth2@gmail.com>
2015-09-26 14:44:28 -0400

    Added the ipv4_read_writer and IPv4_RW

--- ipv4/ipv4_interface.go
+++ ipv4/ipv4_interface.go
@@ -32,3 +32,49 @@ type IPv4_RW interface {
 	ip_write_t
 	ip_close_t
 }
+
+type ipv4_read_writer struct {
+	read *ipv4_reader
+	write *ipv4_writer
+}
+
+func NewIPv4_RW(ip *ipv4tps.IPaddress, protocol uint8) (IPv4_RW, error) {
+	read, err := NewIP_Reader(ip, protocol)
+	if err != nil {
+		return nil, err
+	}
+
+	write, err := NewIP_Writer(ip, protocol)
+	if err != nil {
+		read.Close()
+		return nil, err
+	}
+
+	return &ipv4_read_writer{
+		read: read,
+		write: write,
+	}, nil
+}
+
+func (irw *ipv4_read_writer) ReadFrom() (*IP_Read_Header, error) {
+	return irw.ReadFrom()
+}
+
+func (irw *ipv4_read_writer) WriteTo(data []byte) (int, error) {
+	return irw.write.WriteTo(data)
+}
+
+func (irw *ipv4_read_writer) Close() error {
+	err1 := irw.write.Close()
+	err2 := irw.read.Close()
+
+	if err1 != nil {
+		return err1
+	}
+
+	if err2 != nil {
+		return err2
+	}
+
+	return nil
+}
