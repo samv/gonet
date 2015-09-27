7310ff7c16363be27fded1e6354a841e91c00adc
Harshal Sheth <hsheth2@gmail.com>
2015-09-26 21:41:13 -0400

    Golinted ipv4tps

--- ipv4/ipv4tps/ipv4_types.go
+++ ipv4/ipv4tps/ipv4_types.go
@@ -5,33 +5,40 @@ import (
 	"net"
 )
 
+func init() {
+	allIP := []byte{0, 0, 0, 0}
+	IPAll = &IPAddress{IP: allIP}
+	IPAllHash = IPAll.Hash()
+}
+
 type Netmask uint8
 
 type IPhash uint32
-type IPaddress struct {
+type IPAddress struct {
 	IP []byte
 }
 
-const IPv4_ADDRESS_LENGTH = 4
+const IPv4AddressLength = 4
 
-var all_ip = []byte{0, 0, 0, 0}
-var IP_ALL *IPaddress = &IPaddress{IP: all_ip}
-var IP_ALL_HASH = IP_ALL.Hash()
+var (
+	IPAll *IPAddress
+	IPAllHash IPhash
+)
 
-func (ip *IPaddress) Marshal() ([]byte, error) {
+func (ip *IPAddress) Marshal() ([]byte, error) {
 	return ip.IP, nil
 }
 
-func (ip *IPaddress) Hash() IPhash {
+func (ip *IPAddress) Hash() IPhash {
 	return IPhash(binary.BigEndian.Uint32(ip.IP))
 }
 
-func (ip *IPaddress) Len() uint8 {
-	return IPv4_ADDRESS_LENGTH
+func (ip *IPAddress) Len() uint8 {
+	return IPv4AddressLength
 }
 
-func MakeIP(ip string) *IPaddress {
-	return &IPaddress{
+func MakeIP(ip string) *IPAddress {
+	return &IPAddress{
 		IP: net.ParseIP(ip)[12:],
 	}
 }
