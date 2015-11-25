281a25298fe0334cbcaf95ad163ea7c50fd35f34
Harshal Sheth <hsheth2@gmail.com>
2015-11-25 16:12:02 -0500

    Added internal file descriptor tracker

--- socket.go
+++ socket.go
@@ -1,5 +1,26 @@
 package network
 
+import (
+	"network/ipv4"
+	"sync"
+)
+
+type fd int
+
+var fdManager = new(struct {
+	current fd
+	sync.Mutex
+})
+
+func getFd() fd {
+	fdManager.Lock()
+	defer fdManager.Unlock()
+	fdManager.current++
+	return fdManager.current
+}
+
 type Socket struct {
-	bind func() int
+	fd fd
+
+	Bind   func(port uint16, ip *ipv4.Address) error
 }
