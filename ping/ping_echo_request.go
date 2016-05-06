bf53d79a5b36bfddf68f3982dc28620ed811ddb8
Harshal Sheth <hsheth2@gmail.com>
2016-05-05 22:46:23 -0400

    added a TODO

--- ping/ping_echo_request.go
+++ ping/ping_echo_request.go
@@ -97,7 +97,7 @@ func sequenceDealer(idInput chan *icmp.Packet, seqChan map[uint16](chan *icmp.Pa
 	}
 }
 
-const FLOOD_INTERVAL = 0
+const FLOOD_INTERVAL = 0 // TODO add a flood option
 
 func (pm *Ping_Manager) SendPing(ip *ipv4.Address, interval, timeout time.Duration, numPings uint16) error {
 	terminate := make(chan bool)
