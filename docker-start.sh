9d27788725231c765d7547d542f7b03baac10c5d
Harshal Sheth <hsheth2@gmail.com>
2016-05-05 21:32:32 -0400

    Small enhancement to docker script

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker rm gonet
+
 # should be run in gonet source directory
 docker build -t gonet-linux .
 docker run -it --privileged --name gonet -v `pwd`:/go/src/github.com/hsheth2/gonet gonet-linux:latest
