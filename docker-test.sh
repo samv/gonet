410d9b9b7a8f5daac4ef54d0c3d9b5dafe425606
Harshal Sheth <hsheth2@gmail.com>
2016-05-07 18:31:53 -0400

    Few edits to travis; removed docker-test (not needed)

--- docker-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# should be run in gonet source directory
-docker build -t gonet-linux .
-docker run -it --privileged --rm -v `pwd`:/go/src/github.com/hsheth2/gonet gonet-linux:latest /bin/bash -c "cd /go/src/github.com/hsheth2/gonet && make && make test"
