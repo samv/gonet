808a6a0d3e4ba4a82b93cddd10d2363d67e330f6
Aashish Welling <aashishwelling@gmail.com>
2016-05-07 23:34:04 -0400

    Fixed bounds error from ping IDs

--- ping/ping_echo_request.go
+++ ping/ping_echo_request.go
@@ -90,7 +90,7 @@ func sequenceDealer(idInput chan *icmp.Packet, seqChan [](chan *icmp.Packet), te
 			// /*logs*/logs.Info.Println("icmp in =", packet.Header.Opt)
 			seqNum := uint32(packet.Header.Opt)
 			if seqNum < atomic.LoadUint32(pingCount) - uint32(1) {
-				seqChan[seqNum] <- packet
+				seqChan[seqNum - 1] <- packet
 			} else {
 				/*logs*/ logs.Info.Println("Dropping bad seq num packet with existing identifier")
 			}
@@ -118,9 +118,9 @@ func (pm *Ping_Manager) SendPing(ip *ipv4.Address, interval, timeout time.Durati
 	}
 
 	for ;pingCount <= uint32(numPings); atomic.AddUint32(&pingCount, 1) {
-		seqChannel[pingCount] = make(chan *icmp.Packet)
+		seqChannel[pingCount - 1] = make(chan *icmp.Packet)
 
-		sendSinglePing(writer, id, uint16(pingCount), timeout, seqChannel[pingCount]) // function is non-blocking
+		sendSinglePing(writer, id, uint16(pingCount), timeout, seqChannel[pingCount - 1]) // function is non-blocking
 
 		// not last
 		if uint16(pingCount) != numPings {
